dbd8f9151e08c92d87c521c4e299ec10e88f4cbd
Laura Boltek <boltek.l@gmail.com>
2020-01-21 15:59:57 +0100

    7. dan

--- js/main.js
+++ js/main.js
@@ -3,5 +3,15 @@
         string: 'ondemand',
         adaptiveHeight: true
       });
+
+      var body = $('body');
+
+      $('.btn-toggle-nav').on('click', function() {
+        body.toggleClass('active-nav');
+      });
+
+      $('.nav a').on('click', function() {
+        body.removeClass('active-nav');
+      });
     });
 
